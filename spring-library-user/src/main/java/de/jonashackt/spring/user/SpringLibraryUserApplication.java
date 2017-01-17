6857acbae082099dda5fa88835cefa791123ccf9
Jonas Hecht <jonas.hecht@codecentric.de>
2017-01-17 10:17:55 +0100

    Spring initializr generated projects

---
+++ spring-library-user/src/main/java/de/jonashackt/spring/user/SpringLibraryUserApplication.java
@@ -0,0 +1,12 @@
+package de.jonashackt.spring.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryUserApplication.class, args);
+	}
+}
